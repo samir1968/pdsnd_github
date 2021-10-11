6a77df7d2225bf056687ff406e0d474ac96e6b3f
samir <samirkouiderbouabdallah83@gmail.com>
2021-10-11 21:12:32 +0100

    refactor: Improve input message statement in get_filter() function

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
-        city = input("Which city would you like to explore ?")
+        city = input("PLEASE ENTER OF THE CITY WICH YOU WANT TO EXPLORE: ")
         city = city.lower()
         if city in ['chicago', 'new york city', 'washington']:
             break
